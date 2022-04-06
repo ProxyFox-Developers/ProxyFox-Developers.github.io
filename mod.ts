5e67e2fb8cb8e4912fac5a02e1861045dae108f0
Oliver-makes-code <kookooloopdaking@gmail.com>
2022-04-06 17:28:03 -0500

    Return url used in request

--- mod.ts
+++ mod.ts
@@ -1,17 +1,13 @@
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
-import { h, ssr, tw } from "https://crux.land/nanossr@0.0.1";
+import { ssr } from "https://crux.land/nanossr@0.0.1";
 import * as discord from "https://deno.land/x/harmony@v2.6.0/mod.ts";
 
 const page = await Deno.readTextFile("index.html");
 
-let url = discord.createOAuthURL({
-    "clientID": "947174063841894500",
-    "scopes": ["identify"],
-    "redirectURI": "https://proxyfox.olivermakesco.de"
-});
+let url = "https://discord.com/api/oauth2/authorize?client_id=947174063841894500&redirect_uri=https%3A%2F%2Fproxyfox.olivermakesco.de&response_type=code&scope=identify"
 console.log(url);
 
 await serve((req) => {
     console.log(req);
-    return ssr(() => url);
+    return ssr(() => req.url);
 });
