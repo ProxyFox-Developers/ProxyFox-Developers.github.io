c8cd27f19e4899058e0ff00584d18e9a5d27a686
Oliver-makes-code <kookooloopdaking@gmail.com>
2022-04-06 17:20:46 -0500

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,9 +1,17 @@
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 import { h, ssr, tw } from "https://crux.land/nanossr@0.0.1";
+import * as discord from "https://deno.land/x/harmony@v2.6.0/mod.ts";
 
 const page = await Deno.readTextFile("index.html");
 
+let url = discord.createOAuthURL({
+    "clientID": "947174063841894500",
+    "scopes": ["identify"],
+    "redirectURI": "https://proxyfox.olivermakesco.de"
+});
+console.log(url);
+
 await serve((req) => {
-    const url = new URL(req.url);
-    return ssr(() => page);
+    console.log(req);
+    return ssr(() => url);
 });
