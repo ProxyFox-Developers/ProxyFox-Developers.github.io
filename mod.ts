e37bde438ac4e4fe842bab306aa991754f9453dc
oliver <kookooloopdaking@gmail.com>
2022-06-21 19:45:20 -0500

    Use async in the request handler

--- mod.ts
+++ mod.ts
@@ -2,24 +2,24 @@ import { serve } from "https://deno.land/std@0.114.0/http/server.ts"
 import { ssr } from "https://crux.land/api/get/4cfWmS.ts"
 
 console.log("listening...")
-serve((req) => {
+serve(async (req) => {
     var split = req.url.split("://")[1]
     var url = split.substring(split.indexOf("/"))
     switch (url) {
         case "/index.js":
-            return new Response(Deno.readTextFileSync("./site/index.js"), {
+            return new Response(await Deno.readFile("./site/index.js"), {
                 headers: {
                     "content-type": "text/javascript"
                 }
             })
         case "/index.css":
-            return new Response(Deno.readTextFileSync("./site/index.css"), {
+            return new Response(await Deno.readFile("./site/index.css"), {
                 headers: {
                     "content-type": "text/css"
                 }
             })
         default:
-            return new Response(Deno.readTextFileSync("./site/index.html"), {
+            return new Response(await Deno.readFile("./site/index.html"), {
                 headers: {
                     "content-type": "text/html"
                 }
