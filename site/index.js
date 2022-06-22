024e473bd60b6c37ef3039bb4310a32acc9a3e13
oliver <kookooloopdaking@gmail.com>
2022-06-21 20:17:43 -0500

    Compact code

--- site/index.js
+++ site/index.js
@@ -1,42 +1,28 @@
 let tab = document.getElementsByClassName("pf-tab")[0];
-switch (window.location.pathname) {
-    case "/system":
-        tab.style.left = "12.5vw"
-        break;
-    case "/members":
-        tab.style.left = "25vw"
-        break;
-    case "/switches":
-        tab.style.left = "37.5vw"
-        break;
-    case "/messages":
-        tab.style.left = "50vw"
-        break;
-    case "/settings":
-        tab.style.left = "62.5vw"
-        break;
-}
-function update(name) {
-    if ("/"+name == window.location.pathname) return
-    window.history.pushState({}, "ProxyFox", window.location.protocol+"/"+name)
-    switch (name) {
-        case "system":
+async function updateTab() {
+    switch (window.location.pathname) {
+        case "/system":
             tab.style.left = "12.5vw"
             break;
-        case "members":
+        case "/members":
             tab.style.left = "25vw"
             break;
-        case "switches":
+        case "/switches":
             tab.style.left = "37.5vw"
             break;
-        case "messages":
+        case "/messages":
             tab.style.left = "50vw"
             break;
-        case "settings":
+        case "/settings":
             tab.style.left = "62.5vw"
             break;
         default:
             tab.style.left = "0px"
-            break;
     }
+}
+updateTab()
+setInterval(updateTab, 100)
+function update(name) {
+    if ("/"+name == window.location.pathname) return
+    window.history.pushState({}, "ProxyFox", window.location.protocol+"/"+name)
 }
