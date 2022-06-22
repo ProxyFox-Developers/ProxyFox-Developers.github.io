fa5fb2042a06557cd6a88fe86dd439fe6e3ffe2f
oliver <kookooloopdaking@gmail.com>
2022-06-21 20:12:34 -0500

    Soft redirect page

--- site/index.js
+++ site/index.js
@@ -16,3 +16,26 @@ switch (window.location.pathname) {
         tab.style.left = "62.5vw"
         break;
 }
+function update(name) {
+    window.history.pushState({}, "ProxyFox", window.location.protocol+"/"+name)
+    switch (name) {
+        case "system":
+            tab.style.left = "12.5vw"
+            break;
+        case "members":
+            tab.style.left = "25vw"
+            break;
+        case "switches":
+            tab.style.left = "37.5vw"
+            break;
+        case "messages":
+            tab.style.left = "50vw"
+            break;
+        case "settings":
+            tab.style.left = "62.5vw"
+            break;
+        default:
+            tab.style.left = "0px"
+            break;
+    }
+}
