457febe751c9154c9d80b180b2c983b0396065a3
Octal <oliver@olivermakesco.de>
2022-06-24 11:55:19 -0500

    Get mobile working 100%

--- components/SidebarElement.tsx
+++ components/SidebarElement.tsx
@@ -1,6 +1,12 @@
 export default function SidebarElement({ children, href, selected=false }: { children: React.ReactNode, href: string, selected?: boolean }): JSX.Element {
     return (
-        <a href={href}>
+        <a href={href} onClick={(event) => {
+            if (window.matchMedia('(max-width: 600px)').matches) {
+                event.preventDefault()
+                window.location.href = href+"/hidden"
+                return false
+            }
+        }}>
             <div className={selected? "pf-sidebar element selected": "pf-sidebar element"}>
             <p> { children } </p>
         </div>
--- routes/index.tsx
+++ routes/index.tsx
@@ -18,3 +18,4 @@ export default function Index(): JSX.Element {
         </div>
     );
 }
+
