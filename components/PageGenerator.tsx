a59ab2db9046a22e5b7809f68312df4f38a76b1d
Ampflower <ampflower@outlook.com>
2024-02-03 15:06:34 -0600

    Add deprecation warning
    I pay too much attention to detail :3

--- components/PageGenerator.tsx
+++ components/PageGenerator.tsx
@@ -20,6 +20,19 @@ export default function generatePage(tab?: string, element?: string, hidden = fa
     return (
         <Page hidden={hidden}>
             <PageTitle hidden={hidden}> {page.name} </PageTitle>
+            <div class="deprecation">
+            <h2>⚠ ProxyFox is shutting down</h2>
+            <div class="center">
+            <p>Over time it has become increasingly more obvious that this project isn't sustainable for me anymore<br/>
+               I've been having a hard time finding motivation to work on the bot, and I've been struggling to afford the server I'm renting for it.
+            </p>
+            <h3>ProxyFox will be going offline around <time datetime="2024-03-01T17:00:00Z">Friday, March 1st, 2024 @ 12:00</time></h3>
+            <p>If anyone wants to take over the project, feel free to reach out. Otherwise, any data you haven't exported will be lost.</p>
+            <p>The source code will always remain public on github, at <a href="https://github.com/The-ProxyFox-Group/ProxyFox">https://github.com/The-ProxyFox-Group/ProxyFox</a>.</p>
+            <p>Thanks for everything <img src="https://cdn.discordapp.com/emojis/981046672345944154.webp?size=48" height="24" alt="blobfoxheart" title="blobfoxheart" /><br/>
+               - Vix</p>
+            </div>
+            </div>
             <PageContent hidden={hidden}>
                 {(page.image? <PageContentImage src={page.image} hidden={hidden}></PageContentImage>: <></>)}
                 <PageContentDescription hidden={hidden} image={!page.image}>
